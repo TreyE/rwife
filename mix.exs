9cc00d973ae28b1e510bc3bcc03eca7d6a8753f3
Trey Evans <lewis.r.evans@gmail.com>
2016-06-23 17:02:27 -0400

    Removing poolboy.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Rwife.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger, :poolboy]]
+    [applications: [:logger]]
   end
 
   # Dependencies can be Hex packages:
